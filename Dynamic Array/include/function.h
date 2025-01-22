98a328c4c72fc5473f3d885cb1ff34abbb1faf47
Sumit Sudam Bhor <sumitbhor227@sumit-sanika.com>
2025-01-22 19:40:44 +0530

    Dynamic Array implementation

---
+++ Dynamic Array/include/function.h
@@ -0,0 +1,7 @@
+//#include<stdio.h>
+void create(int **ptrArray, int *ptrSize );
+void read(int * ptrArray, int size);
+void update(int * ptrArray, int size);
+void delete(int **ptrArray, int *size);
+int search(int *ptrArray, int size,int theelement);
+
