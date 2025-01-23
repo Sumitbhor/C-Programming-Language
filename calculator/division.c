2fb6f6ae314020d2410fc85b03a20a3bcbdd5c28
Sumit Sudam Bhor <sumitbhor227@sumit-sanika.com>
2025-01-23 22:17:02 +0530

    calculator implimented

---
+++ calculator/division.c
@@ -0,0 +1,6 @@
+#include"header.h"
+float division(float num1, float num2){
+    float result;
+    result = num1 / num2;
+    return result;
+}
---
+++ calculator/subtraction.c
@@ -0,0 +1,6 @@
+#include"header.h"
+float subtraction(float num1, float num2){
+    float result;
+    result = num1 - num2;
+    return result;
+}
