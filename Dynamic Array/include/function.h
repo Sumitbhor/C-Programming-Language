bc12a1f72d672ee77aadbe849c1fee4be3dd7fc5
Sumit Sudam Bhor <sumitbhor227@gmail.com>
2025-01-28 23:38:09 +0530

    delet

--- Dynamic Array/include/function.h
+++
@@ -1,7 +0,0 @@
-//#include<stdio.h>
-void create(int **ptrArray, int *ptrSize );
-void read(int * ptrArray, int size);
-void update(int * ptrArray, int size);
-void delete(int **ptrArray, int *size);
-int search(int *ptrArray, int size,int theelement);
-
--- calculator/header.h
+++
@@ -1,4 +0,0 @@
-float addition(float num1, float num2);
-float subtraction(float num1, float num2);
-float multiplication(float num1, float num2);
-float division (float num1, float num2);
