f5386e6bd6ea6c9a852feb0470e047ef4010884a
Sumit Sudam Bhor <sumitbhor227@gmail.com>
2025-01-29 16:24:05 +0530

    simple student data management project

---
+++ Student management System/include/data.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int acceptdata();
+int displaydata();
+
+struct Student
+{
+    char name[10] ;
+    int rollno ;
+    int age;
+    int std ;
+    char div[2];
+
+};
+   //struct Student s[totalstudent];
