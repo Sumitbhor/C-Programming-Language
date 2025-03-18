2f4a1743fa0bdde956669f60ef7bb6c4d5390155
Sumit Sudam Bhor <sumitbhor227@gmail.com>
2025-03-18 17:29:26 +0530

    new

--- Basic C codes/starpatt3.c
+++ Basic C codes/starpatt3.c
@@ -4,12 +4,12 @@ int main()
     int i,j ,no,space;
     printf("enter how many lines you want to print");
     scanf("%d",&no);
-    for (int i = 0; i <=no; i++)
+    for (int i = 1; i <=no; i++)
     {
         for(space=no;space>=i;space--){
             printf(" ");
         }
-        for (int j = 0; j <=i; j++)
+        for (int j = 1; j <=i; j++)
         {
             printf("* ");
         }
